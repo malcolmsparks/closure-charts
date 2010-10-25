8202415312bffab242489700c427005d131b15ce
stvhl00@gmail.com <stvhl00@gmail.com>
2010-10-25 11:56:54 +0100

    Changed the name of the directories to reflect the name of the project (javascript-charting was vague and inconsistent with project name)

--- examples/example1/plovr-config.js
+++ examples/example1/plovr-config.js
@@ -1,7 +1,7 @@
 {
 	"id" : "closure-charts-example1",
 	"inputs" : "example1.js",
-	"paths" : "../../javascript-charting/src/main/webapp/js/scottlogic/chart",
+	"paths" : "../../closure-charts/src/main/webapp/js/scottlogic/chart",
 	"closure-library" : "../../closure-library/src/main/webapp/js/goog",
 	"mode" : "ADVANCED",
 	"level" : "VERBOSE",
--- plovr-config.js
+++ plovr-config.js
@@ -1,7 +1,7 @@
 {
 	"id" : "closure-charts",
-	"inputs" : "javascript-charting/src/main/webapp/js/scottlogic/chart/Chart.js",
-	"paths" : "javascript-charting/src/main/webapp/js/scottlogic/chart",
+	"inputs" : "closure-charts/src/main/webapp/js/scottlogic/chart/Chart.js",
+	"paths" : "closure-charts/src/main/webapp/js/scottlogic/chart",
 	"closure-library" : "closure-library/src/main/webapp/js/goog",
 	"mode" : "ADVANCED",
 	"level" : "VERBOSE",
