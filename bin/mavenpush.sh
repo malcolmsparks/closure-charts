63fb878fd3a564d9769e3bf030139dd4d30e1bce
stvhl00@gmail.com <stvhl00@gmail.com>
2010-12-16 11:53:37 +0000

    Moved the VERSION file to the META-INF directory to stop it interfering with dependent projects.

--- bin/mavenpush.sh
+++ bin/mavenpush.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 echo "Creating Maven-friendly directory structure.."
 mkdir -p working/src/main/webapp/js/
+mkdir -p working/src/main/webapp/META-INF/
 cp -r ../closure-charts/src/main/webapp/js/ working/src/main/webapp/
 cp -r ../closure-library/src/main/webapp/js/ working/src/main/webapp/
 cp pom.xml working/pom.xml
-hg tip >> working/src/main/webapp/VERSION.txt
+hg tip >> working/src/main/webapp/META-INF/closure-charts-VERSION.txt
 cd working
 echo "Deploying to Maven"
 mvn deploy
