e0b276dd2a773125c7ee6f5ba5a4ee8ca31e4d34
stvhl00@gmail.com <stvhl00@gmail.com>
2010-12-16 08:56:19 +0000

    Modified the Maven script to push a txt file containing the details of "hg tip" when the push was performed.

--- bin/mavenpush.sh
+++ bin/mavenpush.sh
@@ -4,11 +4,11 @@ mkdir -p working/src/main/webapp/js/
 cp -r ../closure-charts/src/main/webapp/js/ working/src/main/webapp/
 cp -r ../closure-library/src/main/webapp/js/ working/src/main/webapp/
 cp pom.xml working/pom.xml
-echo "Deploying to Maven"
+hg tip >> working/src/main/webapp/VERSION.txt
 cd working
+echo "Deploying to Maven"
 mvn deploy
 echo "Cleaning working directories"
 cd ..
-rm -fr target
 rm -fr working
 
