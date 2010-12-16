d00a52afdeb402af85b9aac7880ad7da8f504595
stvhl00@gmail.com <stvhl00@gmail.com>
2010-12-16 13:24:58 +0000

    Small bugfix to script.

--- bin/mavenpush.sh
+++ bin/mavenpush.sh
@@ -5,7 +5,7 @@ mkdir -p working/src/main/webapp/META-INF/
 cp -r ../closure-charts/src/main/webapp/js/ working/src/main/webapp/
 cp -r ../closure-library/src/main/webapp/js/ working/src/main/webapp/
 cp pom.xml working/pom.xml
-hg tip >> working/src/main/webapp/META-INF/closure-charts-VERSION.txt
+hg tip > working/src/main/webapp/META-INF/closure-charts-VERSION.txt
 cd working
 echo "Deploying to Maven"
 mvn deploy
